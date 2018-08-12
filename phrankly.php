6075472fa79af76e109e608abd00985f82c5c78d
Mabasa, Raphael <raphaelmabasa@gmail.com>
2018-08-12 11:30:30 -0400

    fixed wordpress issues

--- phrankly.php
+++ phrankly.php
@@ -7,13 +7,20 @@
  * Author URI: https://www.phrankly.com
  */
 function phrankly_embed($atts = []){
-    $handle = $atts['handle'];
-    if(empty($handle)) {
-        echo '<p>You must provide a valid Phrankly handle in the form of: [phrankly handle="YourPhranklyHandle"]</p>';
-    } else {
-        $url = 'https://dev.phrankly.com/embed/responsive/' . $handle;
-        echo '<iframe src="' . $url . '" style="border: 2px solid rgb(204, 204, 204); height: 400px; width: 400px;"></iframe>';
-    }
+	$handle = $atts['handle'];
+
+  ob_start();
+
+	if(empty($handle)) {
+		echo '<p>You must provide a valid Phrankly handle in the form of: [phrankly handle="YourPhranklyHandle"]</p>';
+	} else {
+		$url = 'https://dev.phrankly.com/embed/responsive/' . $handle;
+		echo '<iframe src="' . $url . '" style="border: 2px solid rgb(204, 204, 204); height: 400px; width: 400px;"></iframe>';
+	}
+
+  $output = ob_get_clean();
+  //print $output; // debug
+  return $output;
 }
 add_shortcode('phrankly', 'phrankly_embed');
 ?>
