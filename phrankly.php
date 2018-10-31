cf0a263b5405079ee21d04bbcf642054c52d79bc
Brandon Vulaj <brandon.vulaj@gmail.com>
2018-10-30 21:03:45 -0400

    Added map embed option

--- phrankly.php
+++ phrankly.php
@@ -2,17 +2,17 @@
 /*
  * Plugin Name: Phrankly Food and Drink Sourcing
  * Description: Embed Phrankly on your web page
- * Version: 1.1
+ * Version: 1.2
  * Author: Phrankly
  * Author URI: https://www.phrankly.com
  */
-function phrankly_embed($atts = []){
+function phrankly_list($atts = []){
 	$handle = $atts['handle'];
 
   	ob_start();
 
 	if(empty($handle)) {
-		echo '<p>You must provide a valid Phrankly handle in the form of: [phrankly handle="YourPhranklyHandle"]</p>';
+		echo '<p>You must provide a valid Phrankly handle in the form of: [phrankly-list handle="YourPhranklyHandle"]</p>';
 		echo '<p>Please read the Installation instructions for more information.</p>';
 	} else {
 		$url = 'https://www.phrankly.com/embed/responsive/' . $handle;
@@ -22,5 +22,25 @@ function phrankly_embed($atts = []){
 	$output = ob_get_clean();
 	return $output;
 }
-add_shortcode('phrankly', 'phrankly_embed');
+
+function phrankly_map($atts = []){
+	$handle = $atts['handle'];
+
+  	ob_start();
+
+	if(empty($handle)) {
+		echo '<p>You must provide a valid Phrankly handle in the form of: [phrankly-map handle="YourPhranklyHandle"]</p>';
+		echo '<p>Please read the Installation instructions for more information.</p>';
+	} else {
+		$url = 'https://www.phrankly.com/embed/map/' . $handle;
+		echo '<div style="position: relative; overflow: hidden; padding-bottom: 100%;"><iframe src="' . $url . '" style="position: absolute;top: 0; left: 0; width: 100%; height: 100%; border: 0;"></iframe></div>';
+	}
+
+	$output = ob_get_clean();
+	return $output;
+}
+
+add_shortcode('phrankly', 'phrankly_list');
+add_shortcode('phrankly-list', 'phrankly_list');
+add_shortcode('phrankly-map', 'phrankly_map');
 ?>
